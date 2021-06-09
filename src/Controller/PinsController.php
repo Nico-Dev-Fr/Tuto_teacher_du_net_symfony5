69583cb1a27f3ac4a754fd8425e97fd377747a72
admin <admin@vps-7a291c78.vps.ovh.net>
2021-06-09 13:49:53 +0000

    Update

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -8,43 +8,64 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PinsController extends AbstractController
 {
-    #[Route('/', name : 'app_home')]
+    #[Route('/', name : 'app_home', methods: ["GET"])]
     public function index(PinRepository $repo): Response
     {
         return $this->render('pins/index.html.twig', ['pins' => $repo->findAll()]);
-        // $pin = new Pin;
-        // $pin->setTitle('Title 1');
-        // $pin->setDescription('Descrfiption 1');
-
-        // $em = $this->getDoctrine()->getManager();
-        // $em->persist($pin);
-        // $em->flush();
-
-        // $repo = $em->getRepository(Pin::class);
-        // $pins = $repo->findAll();
-        // return $this->render('pins/index.html.twig', compact('pins'));
     }
     
     #[Route('/create', name : 'app_pins_create', methods: ["GET", "POST"])]
     public function create(Request $req, EntityManagerInterface $em){
-        if($req->isMethod('POST')){
-            $data = $req->request->all();
+        // if($req->isMethod('POST')){
+        //     $data = $req->request->all();
 
-            if($this->isCsrfTokenValid('pin_create', $data['_token'])){
-                $pin = new Pin;
-                $pin->setTitle($data['title']);
-                $pin->setDescription($data['description']);
-                $em->persist($pin);
-                $em->flush();
-            }
+        //     if($this->isCsrfTokenValid('pin_create', $data['_token'])){
+        //         $pin = new Pin;
+        //         $pin->setTitle($data['title']);
+        //         $pin->setDescription($data['description']);
+        //         $em->persist($pin);
+        //         $em->flush();
+        //     }
 
-            return $this->redirect($this->generateUrl('app_home'));
+        //     return $this->redirectToRoute('app_home');
+        // }
+
+        $pin = new Pin;
+
+        $form = $this->createFormBuilder($pin)
+            ->add('title', null, [
+                'attr' => [
+                    'autofocus' => true
+                    ] 
+                ])
+            ->add('description', TextareaType::class)
+            // ->add('submit', SubmitType::class, ['label' => 'Create'])
+            ->getForm()
+        ;
+
+        $form->handleRequest($req);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            // $data = $form->getData();
+            
+            // $pin = new Pin;
+            // $pin->setTitle($data['title']);
+            // $pin->setDescription($data['description']);
+            $em->persist($pin);
+            $em->flush();
+
+            return $this->redirectToRoute('app_home');
         }
 
-        return $this->render('pins/create.html.twig');
+        return $this->render('pins/create.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
