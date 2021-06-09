d3a3e6f40586708cfb585197d5c978f5c8d15d2e
admin <admin@vps-7a291c78.vps.ovh.net>
2021-06-09 12:26:11 +0000

    Update

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,19 +2,49 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Pin;
+use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PinsController extends AbstractController
 {
-    #[Route('/')]
-    public function index(): Response
+    #[Route('/', name : 'app_home')]
+    public function index(PinRepository $repo): Response
     {
-        return $this->render('pins/index.html.twig');
-        // return $this->json([
-        //     'message' => 'Welcome to your new controller!',
-        //     'path' => 'src/Controller/PinsController.php',
-        // ]);
+        return $this->render('pins/index.html.twig', ['pins' => $repo->findAll()]);
+        // $pin = new Pin;
+        // $pin->setTitle('Title 1');
+        // $pin->setDescription('Descrfiption 1');
+
+        // $em = $this->getDoctrine()->getManager();
+        // $em->persist($pin);
+        // $em->flush();
+
+        // $repo = $em->getRepository(Pin::class);
+        // $pins = $repo->findAll();
+        // return $this->render('pins/index.html.twig', compact('pins'));
+    }
+    
+    #[Route('/create', name : 'app_pins_create', methods: ["GET", "POST"])]
+    public function create(Request $req, EntityManagerInterface $em){
+        if($req->isMethod('POST')){
+            $data = $req->request->all();
+
+            if($this->isCsrfTokenValid('pin_create', $data['_token'])){
+                $pin = new Pin;
+                $pin->setTitle($data['title']);
+                $pin->setDescription($data['description']);
+                $em->persist($pin);
+                $em->flush();
+            }
+
+            return $this->redirect($this->generateUrl('app_home'));
+        }
+
+        return $this->render('pins/create.html.twig');
     }
 }
