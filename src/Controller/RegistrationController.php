79b0c4d831b7e5553f77639a4585edb0fda2a188
admin <admin@vps-7a291c78.vps.ovh.net>
2021-06-16 18:09:51 +0000

    Add registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -39,19 +39,20 @@ class RegistrationController extends AbstractController
                 )
             );
 
+            // dd($user);
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('noreply@nicolascollot.fr', 'Mail Bot'))
-                    ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
-            // do anything else you need here, like send an email
+            // // generate a signed url and email it to the user
+            // $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+            //     (new TemplatedEmail())
+            //         ->from(new Address('noreply@nicolascollot.fr', 'Mail Bot'))
+            //         ->to($user->getEmail())
+            //         ->subject('Please Confirm your Email')
+            //         ->htmlTemplate('registration/confirmation_email.html.twig')
+            // );
+            // // do anything else you need here, like send an email
 
             return $this->redirectToRoute('app_home');
         }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Form\RegistrationFormType;
 
 class SecurityController extends AbstractController
 {
-    /**
-     * @Route("/login", name="app_login")
-     */
+    #[Route('/login', name : 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
@@ -26,18 +28,40 @@ class SecurityController extends AbstractController
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
-
-    /**
-     * @Route("/logout", name="app_logout")
-     */
+    
+    #[Route('/logout', name : 'app_logout')]
     public function logout()
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
-
-    #[Route('/account', name : 'app_user_edit', methods: ["GET"])]
-    public function edit(UserRepository $repo): Response
+    
+    #[Route('/account', name : 'app_account_edit')]
+    public function edit(Request $req, EntityManagerInterface $em) : Response
     {
-        return $this->render('users/edit.html.twig');
+        $form = $this->createForm(RegistrationFormType::class, $this->getUser());
+
+        $form->handleRequest($req);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            $em->flush();
+            $this->addFlash('success','Successfull update');
+
+            return $this->redirectToRoute('app_account_edit');
+        }
+
+        return $this->render('account/edit.html.twig', [
+            'user' => $this->getUser(),
+            'form' => $form->createView()
+        ]);
+    }
+        
+    #[Route('/account_delete', name : 'app_account_delete')]
+    public function delete(Request $req, EntityManagerInterface $em) : Response
+    {
+        $em->remove($this->getUser());
+        $em->flush();
+        $this->addFlash('info','Successfull delete');
+
+        return $this->redirectToRoute('app_login');
     }
 }
